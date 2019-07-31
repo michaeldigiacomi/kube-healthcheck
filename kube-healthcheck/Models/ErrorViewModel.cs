921e75bd9a56d58e6ee903a25619109703dd4708
Michael Di Giacomi <digitaladrenalin@gmail.com>
2019-07-31 16:01:59 -0400

    Draft create

---
+++ kube-healthcheck/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kube_healthcheck.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
